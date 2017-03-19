d5b5c48af03c37b36dffbf0afd9dea86212ee7bd
shifasabaat <psychology.thought@gmail.com>
2017-03-19 16:18:39 -0700

    Update date.h

--- date.h
+++ date.h
@@ -8,70 +8,15 @@ private:
 	int day, month, year;
 public:
 	Date();
-	Date(int day, int month, int year);
-	void setDate(int dy, int mon, int yr)
-	{
-		day = dy;
-		month = mon;
-		year = yr;
-	}
-	bool Date::operator > (const Date &dateobj)
-	{
-		bool status;
-		if (year < dateobj.year)
-			status = true;
-		else if (year > dateobj.year)
-			status = false;
-		else if (year == dateobj.year)
-		{
-			if (month < dateobj.month)
-				status = true;
-			else if (month > dateobj.month)
-				status = false;
-			else if (month == dateobj.month)
-			{
-				if (day < dateobj.day)
-					status = true;
-				else if (day >= dateobj.day)
-					status = false;
-			}
-		}
-		return status;
-	}
-
-	bool Date::operator ==(const Date &dateobj)
-	{
-		bool status;
-		if (year == dateobj.year && month == dateobj.month && day == dateobj.day)
-			status = true;
-		else
-			status = false;
-		return status;
-	}
-
-	bool Date::operator <(const Date &dateobj)
-	{
-		bool status;
-		if (year > dateobj.year)
-			status = true;
-		else if (year < dateobj.year)
-			status = false;
-		else if (year == dateobj.year)
-		{
-			if (month > dateobj.month)
-				status = true;
-			else if (month < dateobj.month)
-				status = false;
-			else if (month == dateobj.month)
-			{
-				if (day > dateobj.day)
-					status = true;
-				else if (day <= dateobj.day)
-					status = false;
-			}
-		}
-		return status;
-	}
+	void setDate(string date);
+	void setDate(int dy, int mon, int yr);
+	bool operator > (const Date &dateobj);
+	bool operator ==(const Date &dateobj);
+	bool operator <(const Date &dateobj);
+	void errorLoop(); //<- this will be checking day, month, and year bounds
+	istream operator >>(const Date &dateobj);
+	ostream operator <<(const Date &dateobj);
+	
 
 
 };
