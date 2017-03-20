47d686af900b57fd20eabf61ec93ece130868668
shifasabaat <psychology.thought@gmail.com>
2017-03-20 01:45:04 -0700

    Update reportmodule.h

--- reportmodule.h
+++ reportmodule.h
@@ -1,33 +1,76 @@
-#ifndef REPORT_MODULE
-#define REPORT_MODULE
+#define _CRT_SECURE_NO_WARNINGS
+#ifndef REPORTINGMODULE_H
+#define REPORTINGMODULE_H
+#include "InventoryModule.h"
+#include "Module.h"
 
+using namespace std;
 
-class ReportModule : public Module
+class ReportingModule : public Module
 {
-    
-    
-    
+protected:
+	InventoryModule *inventory;
 public:
-    ReportModule(BookList*);
-    void act();
-    string sortBytitle();
-    string sortBywholesaleprice();
-    string sortByretailpricee();
-    string sortByquantity();
-    string sortBycost();
-    string sortBydate();
-    
-    
-private:
-    const static int sortBytitle = 1,
-                     sortBywholesaleprice = 2,
-                     sortByretailpricee = 3,
-                     sortByquantity = 4,
-                     sortBycost = 5,
-                     sortBydate = 6;
-    
-    void sort(int st);
-    
-};
+	ReportingModule(InventoryModule *inv) : Module()
+	{
+		inventory = inv;
+	}
+	void driver()
+	{
+		int choice;
+		do
+		{
+			choice = displayRepenu();
+			switch (choice)
+			{
+			case 1:{
+					   inventory->sortBytitle();
+					   inventory->displayBooks();
+			}
+				break;
+			case 2:{
+					   inventory->sortByquantity();
+					   inventory->displayBooks();
+			}
+				break;
+			case 3:{
+					   inventory->sortByRetailPrice();
+					   inventory->displayBooks();
+			}
+				break;
+			case 4:{
+					   cout << "This option is currently unavailable because sortByAge has not yet been completed.\n\n";
+					   //inventory->sortByAge();
+					   //inventory->displayBooks();
+			}
+				break;
+			case 5:
+				cout << "This option is currently unavailable because totalInventoryWholesale is incomplete.\n\n";
+				break;
+			case 6:
+				cout << "This option is currently unavailable because totalInventoryRetail is incomplete.\n\n";
+				break;
+			default:
+				cout << "Invalid choice. Please try again:";
+			}
 
+		} while (choice != 7);
+	}
+	int displayRepenu()
+	{
+		int entry;
+		cout << "\n\n\n\nReporting Module Menu:\n";
+		cout << "1) Listing By Title\n";
+		cout << "2) Listing By Quantity\n";
+		cout << "3) Listing By Cost\n";
+		cout << "4) Listing By Age\n";
+		cout << "5) Total Inventory Wholesale Value\n";
+		cout << "6) Total Inventory Retail Value\n";
+		cout << "7) Return to Main Menu\n\n";
+		cout << "Enter your choice: ";
+		cin >> entry;
+		return entry;
+	}
+
+};
 #endif
