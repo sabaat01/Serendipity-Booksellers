a53acc6443b98c979dace2b6860e4e5501138892
shifasabaat <psychology.thought@gmail.com>
2017-03-20 01:44:13 -0700

    Update module.h

--- module.h
+++ module.h
@@ -1,1 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#ifndef MODULE_H
+#define MODULE_H
+//Our abstract base class
+#include <iostream>
 
+using namespace std;
+
+class Module
+{
+public:
+	Module()
+	{
+		cout << "\nA Module instance or instance of one of the inherited classes has just been created.\n\n";
+	}
+
+	virtual void driver() = 0;
+};
+#endif
