9b316854f9870baba1a7286c144aaeaefc465854
shifasabaat <psychology.thought@gmail.com>
2017-03-20 01:39:07 -0700

    Update book.h

--- book.h
+++ book.h
@@ -1,59 +1,100 @@
 #define _CRT_SECURE_NO_WARNINGS
-//this is the book class
-//it contains member variables and accessors and mutators for the variables. 
-//An array of books will be created- you must ensure the program properly accesses all book values.
 #ifndef BOOK_H
 #define BOOK_H
 #include "Date.h"
 #include <string>
 
+using namespace std;
+
 class Book
 {
 private: //member variables
-Date dateAdded;
-string title;
-string author;
-string publisher;
-unsigned long long int ISBN;
-int quantity;
-double wholesale;
-double retail;
+	Date dateAdded;
+	string title;
+	string author;
+	string publisher;
+	string ISBN;
+	int quantity;
+	double wholesale;
+	double retail;
 
 public: //member functions
-//Setters
-void setTitle(string s)
-{ title = s; }
-void setAuthor(string a)
-{ author = a; }
-void setISBN(unsigned long long int isbn)
-{ ISBN = isbn; }
-void setPublisher(string p)
-{ publisher = p; }
-void setWholesale(double whole)
-{ wholesale = whole; }
-void setRetail(double ret)
-{ retail = ret; }
-void setDate(int month, int day, int year)
-{ 
-dateAdded.setMonth(month);
-dateAdded.setDay(day);
-dateAdded.setYear(year);
-}
-void setQuantity(int q)
-{ quantity = q; }
+	//Setters
+	Book()
+	{
+		retail = 0;
+		dateAdded.setDate(0, 0, 0);
+	}
+	void setTitle(string s)
+	{
+		title = s;
+	}
+	void setAuthor(string a)
+	{
+		author = a;
+	}
+	void setISBN(string isbn)
+	{
+		ISBN = isbn;
+	}
+	void setPublisher(string p)
+	{
+		publisher = p;
+	}
+	void setWholesale(double whole)
+	{
+		wholesale = whole;
+	}
+	void setRetail(double ret)
+	{
+		retail = ret;
+	}
+	void setDate(int month, int day, int year)
+	{
+		dateAdded.setDate(day, month, year);
+	}
+	void setDate(string date)
+	{
+		dateAdded.setDate(date);
+	}
+	void setQuantity(int q)
+	{
+		quantity = q;
+	}
 
-//Getters
-string getTitle()
-{ return title; }
-string getAuthor()
-{ return author; }
-unsigned long long int getISBN();
-string getPublisher();
-double getWholesale();
-double getRetail();
-Date getDate();
-int getQuantity();
-
-
+	//Getters
+	string getTitle()
+	{
+		return title;
+	}
+	string getAuthor()
+	{
+		return author;
+	}
+	string getISBN()
+	{
+		return ISBN;
+	}
+	string getPublisher()
+	{
+		return publisher;
+	}
+	double getWholesale()
+	{
+		return wholesale;
+	}
+	double getRetail()
+	{
+		return retail; 
+	}
+	void getDate()
+	{
+		cout << dateAdded;//dateAdded.getDate();
+	}
+	int getQuantity()
+	{
+		return quantity;
+	}
 };
 
+#endif
