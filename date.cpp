be4b9af88c50208cdd3326e93c479595beda6a54
shifasabaat <psychology.thought@gmail.com>
2017-03-21 22:06:58 -0700

    Update date.cpp

--- date.cpp
+++ date.cpp
@@ -1,26 +1,32 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "Date.h"
 
-Date::Date()
-{
-  day = 00;
-  month = 00;
-  year = 0000;
-}
-void Date::setDate(string date)
-{
-		std::string mon, dy, yr;
-		mon = stringformat.substr(0, 2);
-		dy = stringformat.substr(3, 2);
-		yr = stringformat.substr(6, 4);
-		setDate(stoi(mon), stoi(dy), stoi(yr));
-}
+	Date::Date()
+	{
+		day = 0;
+		month = 0;
+		year = 0;
+	}
+	Date::Date(int dy, int mon, int yr)
+	{
+		day = dy;
+		month = mon;
+		year = yr;
+	}
 	void Date::setDate(int dy, int mon, int yr)
 	{
 		day = dy;
 		month = mon;
 		year = yr;
 	}
+	void Date::setDate(std::string stringformat)
+	{
+		stringstream stream;
+		stream << stringformat;
+		stream >> *this;
+
+	}
+
 	bool Date::operator > (const Date &dateobj)
 	{
 		bool status;
@@ -79,26 +85,22 @@ void Date::setDate(string date)
 		return status;
 	}
 
-	istream operator >>(const Date &dateobj)
-  {
-  		//For inputting the Date from the file to the inventory
-		//also for entering the Date manually
-		string input;
-		cin >> input;
-		//For the sake of error handling:
-		if (input.length() != 11)
-			input = "00/00/0000";
-		string tempMonth = input.substr(0, 2);
-		string tempDay = input.substr(3, 2);
-		string tempYear = input.substr(6, 4);
-		dateobj.month = stoi(tempMonth);
-		dateobj.day = stoi(tempDay);
-		dateobj.year = stoi(tempYear);
-		return cin;
-  }
-	ostream operator <<(const Date &dateobj)
-  {
-  	cout << dateobj.day << "/" << dateobj.month << "/" << dateobj.year;
-    return cout; // <-what the hell is this again? Check the textbook...
-  }
+	ostream &operator <<(ostream &stra, const Date &dateobj)
+{
+	stra << dateobj.month << '/' << dateobj.day << '/' << dateobj.year;
 
+	return stra;
+}
+
+istream &operator >>(istream &strm, Date &date) {
+	int month, day, year;
+	strm >> month;
+	strm.ignore();
+	strm >> day;
+	strm.ignore();
+	strm >> year;
+	strm.ignore();
+
+	date.setDate(day, month, year);
+	return strm;
+}
