5ea968b6c197f42cac921007dcc9ab8b3659e435
shifasabaat <psychology.thought@gmail.com>
2017-03-21 21:44:51 -0700

    Create book.cpp

---
+++ book.cpp
@@ -0,0 +1,79 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Book.h"
+
+Book::Book()
+	{
+		retail = 0;
+		dateAdded.setDate(0, 0, 0);
+	}
+void Book::setTitle(string s)
+	{
+		title = s;
+	}
+void Book::setAuthor(string a)
+	{
+		author = a;
+	}
+void Book::setISBN(string isbn)
+	{
+		ISBN = isbn;
+	}
+void Book::setPublisher(string p)
+	{
+		publisher = p;
+	}
+void Book::setWholesale(double whole)
+	{
+		wholesale = whole;
+	}
+void Book::setRetail(double ret)
+	{
+		retail = ret;
+	}
+void Book::setDate(int month, int day, int year)
+	{
+		dateAdded.setDate(day, month, year);
+	}
+void Book::setDate(string date)
+	{
+		dateAdded.setDate(date);
+	}
+void Book::setQuantity(int q)
+	{
+		quantity = q;
+	}
+
+	//Getters
+string Book::getTitle()
+	{
+		return title;
+	}
+string Book::getAuthor()
+	{
+		return author;
+	}
+string Book::getISBN()
+	{
+		return ISBN;
+	}
+string Book::getPublisher()
+	{
+		return publisher;
+	}
+double Book::getWholesale()
+	{
+		return wholesale;
+	}
+double Book::getRetail()
+	{
+	return wholesale * MARKUP;
+	}
+void Book::getDate()
+	{
+		cout << dateAdded;//dateAdded.getDate();
+	}
+int Book::getQuantity()
+	{
+		return quantity;
+	}
+void Book::setDate(const Date &d) { dateAdded = d; }
