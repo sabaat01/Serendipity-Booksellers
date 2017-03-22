365fe96ac4542f75ecd0c8b1347e497886e4d76e
shifasabaat <psychology.thought@gmail.com>
2017-03-21 21:38:06 -0700

    Update book.h

--- book.h
+++ book.h
@@ -1,4 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
 #ifndef BOOK_H
 #define BOOK_H
 #include "Date.h"
@@ -17,84 +16,31 @@ private: //member variables
 	int quantity;
 	double wholesale;
 	double retail;
+	double MARKUP = 1;
 
 public: //member functions
 	//Setters
-	Book()
-	{
-		retail = 0;
-		dateAdded.setDate(0, 0, 0);
-	}
-	void setTitle(string s)
-	{
-		title = s;
-	}
-	void setAuthor(string a)
-	{
-		author = a;
-	}
-	void setISBN(string isbn)
-	{
-		ISBN = isbn;
-	}
-	void setPublisher(string p)
-	{
-		publisher = p;
-	}
-	void setWholesale(double whole)
-	{
-		wholesale = whole;
-	}
-	void setRetail(double ret)
-	{
-		retail = ret;
-	}
-	void setDate(int month, int day, int year)
-	{
-		dateAdded.setDate(day, month, year);
-	}
-	void setDate(string date)
-	{
-		dateAdded.setDate(date);
-	}
-	void setQuantity(int q)
-	{
-		quantity = q;
-	}
+	Book();
+	void setTitle(string s);
+	void setAuthor(string a);
+	void setISBN(string isbn);
+	void setPublisher(string p);
+	void setWholesale(double whole);
+	void setRetail(double ret);
+	void setDate(int month, int day, int year);
+	void setDate(string date);
+	void setQuantity(int q);
 
 	//Getters
-	string getTitle()
-	{
-		return title;
-	}
-	string getAuthor()
-	{
-		return author;
-	}
-	string getISBN()
-	{
-		return ISBN;
-	}
-	string getPublisher()
-	{
-		return publisher;
-	}
-	double getWholesale()
-	{
-		return wholesale;
-	}
-	double getRetail()
-	{
-		return retail; 
-	}
-	void getDate()
-	{
-		cout << dateAdded;//dateAdded.getDate();
-	}
-	int getQuantity()
-	{
-		return quantity;
-	}
+	string getTitle();
+	string getAuthor();
+	string getISBN();
+	string getPublisher();
+	double getWholesale();
+	double getRetail();
+	void getDate();
+	int getQuantity();
+	void setDate(const Date &d);
 };
 
 #endif
